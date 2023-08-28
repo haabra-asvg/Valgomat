a362b4b98810540cf134901f862201801a8e84b5
haabra-asvg <e-haabra2@iskule.no>
2023-08-28 20:33:38 +0200

    Init

---
+++ js/index.js
@@ -0,0 +1,172 @@
+var spørsmål = {
+  0: {
+      "spørsmål": "Spørsmål 1",
+      "enighet": {
+          "Helt enig": [ "Høyre" ],
+          "Litt enig": [ "Vemstre" ],
+          "Litt uenig": [ "SV" ],
+          "Helt uenig": [ "Høyre" ]
+      }
+  },
+
+  1: {
+      "spørsmål": "Spørsmål 2",
+      "enighet": {
+          "Helt enig": [ "SV" ],
+          "Litt enig": [ "Venstre" ],
+          "Litt uenig": [ "Høyre" ],
+          "Helt uenig": [ "Venstre" ]
+      }
+  }
+};
+
+const partier = [
+  { "navn": "Høyre", "poeng": 0 },
+  { "navn": "Venstre", "poeng": 0 },
+  { "navn": "SV", "poeng": 0 }
+
+];
+
+const felt = document.getElementById("spørsmålsfelt");
+const choices = document.querySelectorAll(".choice");
+const btn = document.getElementById("submit-btn");
+
+var spørsmålsnummer = 0;
+var svar = [];
+
+felt.innerHTML = spørsmål[spørsmålsnummer].spørsmål;
+
+// SUBMITTION CORE
+
+function submitted(value) {
+
+for (const choice of choices) {
+  if(choice.id == value) {
+    
+    svar.push(choice.value);
+    partier.forEach(parti => {
+      if(Object.keys(spørsmål[spørsmålsnummer].enighet).includes(choice.value)) {
+          for(let i = 0; i < spørsmål[spørsmålsnummer].enighet[choice.value].length; i++) {
+              if(spørsmål[spørsmålsnummer].enighet[choice.value][i] == parti.navn) {
+                  parti.poeng++;
+              }
+          }
+      }
+    });
+
+    spørsmålsnummer++;
+
+    // HIDE STUFF
+
+    if(spørsmålsnummer > Object.keys(spørsmål).length - 1) {
+      felt.style.display = "none";
+      choices.forEach(choice => {
+        choice.style.display = "none";
+      });
+
+      // TEKST
+
+      const container = document.querySelector(".spørsmål-container");
+      const tekst = document.createElement("h1");
+      tekst.setAttribute("class", "resultatTekst")
+      tekst.innerHTML = "Resultat";
+      container.appendChild(tekst);
+
+      partier
+
+      // CREATE FULL PROGRESS BAR
+
+      partier.forEach(parti => {
+
+        // CREATE PROGRESS TEXT
+
+        const progressTekst = document.createElement("div");
+        progressTekst.setAttribute("class", "partiTekst");
+        progressTekst.innerHTML = parti.navn;
+        container.appendChild(progressTekst);
+
+        // CREATE PROGRESS DIV
+
+        const progressDiv = document.createElement("div");
+        progressDiv.setAttribute("class", "progress");
+        progressDiv.style.width = "90%";
+        progressDiv.style.margin = "auto";
+        container.append(progressDiv);
+
+        // CREATE PROGRESS BAR
+
+        const progressBar = document.createElement("div");
+        progressBar.setAttribute("class", "progress-bar", "role", "progressbar", "aria-valuenow", (parti.poeng / Object.keys(spørsmål).length) * 100, "aria-valuemin", "0", "aria-valuemax", "100");
+        progressBar.style.width = (parti.poeng / Object.keys(spørsmål).length) * 100 + "%";
+        progressBar.innerHTML = (parti.poeng / Object.keys(spørsmål).length) * 100 + "%";
+        progressDiv.appendChild(progressBar);
+      });
+
+      // CREATE BUTTON
+
+      const restartButton = document.createElement("button");
+      restartButton.classList.add("restart");
+      restartButton.setAttribute("id", "restart-btn");
+      restartButton.setAttribute("onclick", "restartButton()");
+      restartButton.style.display = "block";
+      restartButton.style.marginLeft = "auto";
+      restartButton.style.marginRight = "auto";
+      restartButton.style.marginTop = "3%";
+      restartButton.style.marginBottom = "3%";
+      restartButton.innerHTML = "Start på nytt";
+      container.appendChild(restartButton);
+
+    } else {
+      felt.innerHTML = spørsmål[spørsmålsnummer].spørsmål;
+    }
+
+  }
+}
+
+}
+
+function startButton() {
+  document.querySelector(".start").style.display = "none";
+  document.querySelector(".valgomat-container").style.display = "block";
+  const tittel = document.querySelector(".tittel");
+  tittel.style.display = "none";
+  const overFlowEllipsis = document.querySelector(".overflow-ellipsis");
+  overFlowEllipsis.style.paddingTop = "2.4%";
+  overFlowEllipsis.style.paddingBottom = "2.4%";
+}
+
+function restartButton() {
+  for(const parti in partier) {
+    partier[parti].poeng = 0;
+  }
+
+  const progressTekst = document.querySelectorAll(".partiTekst");
+  const progressBarer = document.querySelectorAll(".progressBarer");
+  const resultatTekst = document.querySelector(".resultatTekst");
+  const restartKnapp = document.getElementById("restart-btn");
+  const progressDiv = document.querySelectorAll(".progress");
+
+  restartKnapp.remove(); 
+  resultatTekst.remove();
+
+  progressTekst.forEach(tekst => {
+    tekst.remove();
+  });
+
+  progressBarer.forEach(bar => {
+    bar.remove();
+  });
+
+  progressDiv.forEach(choice => {
+    choice.remove();
+  });
+
+  choices.forEach(choice => {
+    choice.style.display = "block";
+  });
+
+  felt.style.display = "block";
+  spørsmålsnummer = 0;
+  svar = [];
+  felt.innerHTML = spørsmål[spørsmålsnummer].spørsmål;
+}
