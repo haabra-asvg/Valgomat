d667fdcaa5645c62730375378068319017fe4a39
haabra-asvg <e-haabra2@iskule.no>
2023-09-11 13:16:28 +0200

    QoL kode endringer

--- js/index.js
+++ js/index.js
@@ -2,39 +2,55 @@ var spørsmål = {
   0: {
       "spørsmål": "Spørsmål 1",
       "enighet": {
-          "Helt enig": [ "Høyre" ],
-          "Litt enig": [ "Venstre" ],
-          "Litt uenig": [ "SV" ],
-          "Helt uenig": [ "Høyre" ]
+          "Helt enig": [ "Høyre", "AP", "SV" ],
+          "Litt enig": [ "Venstre", "KrF", "FrP" ],
+          "Litt uenig": [ "Rødt", "MDG" ],
+          "Helt uenig": [ "SP" ]
       }
   },
 
   1: {
       "spørsmål": "Spørsmål 2",
       "enighet": {
-          "Helt enig": [ "SV" ],
-          "Litt enig": [ "Venstre" ],
-          "Litt uenig": [ "Høyre" ],
-          "Helt uenig": [ "Venstre" ]
+          "Helt enig": [ "SV", "FrP" ],
+          "Litt enig": [ "Venstre", "Rødt", "SP", "MDG" ],
+          "Litt uenig": [ "Høyre", "AP" ],
+          "Helt uenig": [ "KrF" ]
       }
   },
 
   2: {
       "spørsmål": "Spørsmål 3",
       "enighet": {
-          "Helt enig": [ "Høyre" ],
-          "Litt enig": [ "Venstre" ],
-          "Litt uenig": [ "SV" ],
-          "Helt uenig": [ "Høyre" ]
+          "Helt enig": [ "AP", "MDG", "KrF" ],
+          "Litt enig": [ "FrP", "Venstre", "SV" ],
+          "Litt uenig": [ "SP", "Høyre" ],
+          "Helt uenig": [ "Rødt" ]
+      }
+  },
+
+  3: {
+      "spørsmål": "Spørsmål 4",
+      "enighet": {
+          "Helt enig": [ "Høyre", "FrP" ],
+          "Litt enig": [ "Venstre", "KrF", "AP" ],
+          "Litt uenig": [ "SV", "SP", "MDG" ],
+          "Helt uenig": [ "Rødt" ]
       }
   }
 
 };
 
 const partier = [
-  { "navn": "Høyre", "poeng": 0 },
-  { "navn": "Venstre", "poeng": 0 },
-  { "navn": "SV", "poeng": 0 }
+  { "navn": "Rødt", "alias": "Rødt", "poeng": 0, "partiFarge": "#B22222" },
+  { "navn": "Sosialistisk Venstreparti", "alias": "SV", "poeng": 0, "partiFarge": "#440C1A" },
+  { "navn": "Arbeiderpartiet", "alias": "AP", "poeng": 0, "partiFarge": "#d70926" },
+  { "navn": "Senterpartiet", "alias": "SP", "poeng": 0, "partiFarge": "#78BE21" },
+  { "navn": "Miljøpartiet de grønne", "alias": "MDG", "poeng": 0, "partiFarge": "#4e7e00" },
+  { "navn": "Kristi folkeparti", "alias": "KrF", "poeng": 0, "partiFarge": "#db3a33" },
+  { "navn": "Venstre", "alias": "Venstre", "poeng": 0, "partiFarge": "#066" },
+  { "navn": "Høyre", "alias": "Høyre", "poeng": 0, "partiFarge": "#0065f1" },
+  { "navn": "Fremskrittspartiet", "alias": "FrP", "poeng": 0, "partiFarge": "#003955" },
 
 ];
 
@@ -44,6 +60,7 @@ const btn = document.getElementById("submit-btn");
 
 var spørsmålsnummer = 0;
 var svar = [];
+// var svarids = [];
 
 felt.innerText = spørsmål[spørsmålsnummer].spørsmål;
 
@@ -53,21 +70,42 @@ function submitted(value) {
 
 for (const choice of choices) {
   if(choice.id == value) {
+
+    // if(svarids[spørsmålsnummer]) {
+    //   var prevchoice = document.getElementById(svarids[spørsmålsnummer]);
+    //   prevchoice.style.opacity = "1";
+    //   partier.forEach(parti => {
+    //     if(Object.keys(spørsmål[spørsmålsnummer].enighet).includes(choice.value)) {
+    //         for(let i = 0; i < spørsmål[spørsmålsnummer].enighet[choice.value].length; i++) {
+    //             if(spørsmål[spørsmålsnummer].enighet[choice.value][i] == parti.alias) {
+    //                 parti.poeng--;
+    //             }
+    //         }
+    //     }
+    //   });
+    //   svarids.splice(spørsmålsnummer, 1);
+    //   svar.splice(spørsmålsnummer, 1);
+    // }
     
+    // svarids.push(choice.id);
     svar.push(choice.value);
+
     partier.forEach(parti => {
-      if(Object.keys(spørsmål[spørsmålsnummer].enighet).includes(choice.value)) {
-          for(let i = 0; i < spørsmål[spørsmålsnummer].enighet[choice.value].length; i++) {
-              if(spørsmål[spørsmålsnummer].enighet[choice.value][i] == parti.navn) {
-                  parti.poeng++;
+
+      // POENG CORE
+
+      if(Object.keys(spørsmål[spørsmålsnummer].enighet).includes(choice.value)) { // Sjekker om knappen valgt er en av de 4 knappene
+          for(let i = 0; i < spørsmål[spørsmålsnummer].enighet[choice.value].length; i++) { // Skaffer alle partiene som er enige med valget
+              if(spørsmål[spørsmålsnummer].enighet[choice.value][i] == parti.alias) { // Dobbeltsjekker om aliasen til partiet som er enig stemmer
+                  parti.poeng++; // Legger til 1 poeng til partiet
               }
           }
       }
     });
 
-    spørsmålsnummer++;
+    spørsmålsnummer++; // Øker spørsmålsnummeret med 1
 
-    // HIDE STUFF
+    // HIDE BTNS + QUESTION
 
     if(spørsmålsnummer > Object.keys(spørsmål).length - 1) {
       felt.style.display = "none";
@@ -97,6 +135,11 @@ for (const choice of choices) {
 
         const progressTekst = document.createElement("div");
         progressTekst.setAttribute("class", "partiTekst");
+        progressTekst.style.fontWeight = "bold";
+        progressTekst.style.fontSize = "22px";
+        progressTekst.style.marginBottom = "3%";
+        progressTekst.style.marginTop = "3%";
+        progressTekst.style.color = parti.partiFarge;
         progressTekst.innerText = parti.navn;
         container.appendChild(progressTekst);
 
@@ -108,12 +151,18 @@ for (const choice of choices) {
         progressDiv.style.margin = "auto";
         container.append(progressDiv);
 
+        // INCREASE PAGE SIZE
+
+        const wrapper = document.querySelector(".wrapper");
+        wrapper.style.height = "102%";
+
         // CREATE PROGRESS BAR
 
         const progressBar = document.createElement("div");
         progressBar.setAttribute("class", "progress-bar", "role", "progressbar", "aria-valuenow", (parti.poeng / Object.keys(spørsmål).length) * 100, "aria-valuemin", "0", "aria-valuemax", "100");
         progressBar.style.width = "0%";
         progressBar.innerText = "0%";
+        progressBar.style.backgroundColor = parti.partiFarge;
         progressDiv.appendChild(progressBar);
         setTimeout(() => {
           progressBar.style.transition = "width 1s ease-in-out";
@@ -165,6 +214,9 @@ function restartButton() {
   const resultatTekst = document.querySelector(".resultatTekst");
   const restartKnapp = document.getElementById("restart-btn");
   const progressDiv = document.querySelectorAll(".progress");
+  const wrapper = document.querySelector(".wrapper");
+
+  wrapper.style.height = "100vh";
 
   restartKnapp.remove(); 
   resultatTekst.remove();
@@ -188,6 +240,7 @@ function restartButton() {
   felt.style.display = "block";
   spørsmålsnummer = 0;
   svar = [];
+  // svarids = [];
   felt.innerText = spørsmål[spørsmålsnummer].spørsmål;
 }
 
@@ -202,3 +255,36 @@ function onLoad() {
 
 
 }
+
+// function arrowBack() {
+//   if(spørsmålsnummer > 0) {
+//     spørsmålsnummer--;
+//     felt.innerText = spørsmål[spørsmålsnummer].spørsmål;
+//     if(svarids[spørsmålsnummer]) {
+//       var choice = document.getElementById(svarids[spørsmålsnummer]);
+//       choice.style.opacity = "0.7";
+//     } else {
+//       choices.forEach(choice => {
+//         choice.style.opacity = "1";
+//       });
+//     }
+//   }
+// }
+
+// function arrowForward() {
+//   if(spørsmålsnummer < Object.keys(spørsmål).length - 1) {
+//     choices.forEach(choice => {
+//       choice.style.opacity = "1";
+//     });
+//     spørsmålsnummer++;
+//       felt.innerText = spørsmål[spørsmålsnummer].spørsmål;
+//     if(svarids[spørsmålsnummer]) {
+//       var choice = document.getElementById(svarids[spørsmålsnummer]);
+//       choice.style.opacity = "0.7";
+//     } else {
+//       choices.forEach(choice => {
+//         choice.style.opacity = "1";
+//       });
+//     }
+//   }
+// }
